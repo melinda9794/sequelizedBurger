8970869ca0b9985f6b800253049dc217298ed163
Melinda <melinda9794@gmail.com>
2017-04-20 08:56:34 -0400

    progress

---
+++ models/devourers.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var devourers = sequelize.define('devourers', {
+    devourer_name: DataTypes.STRING,
+    burgerId: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        devourers.belongsTo(models.burgers);
+      }
+    }
+  });
+  return devourers;
+};
